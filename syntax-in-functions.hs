4979f9d7a3316d46fb76afcf5f9d7e8d0ae3ccb7
Anton Logvinenko <logvinenko@farpost.com>
2012-05-22 00:03:26 +1100

    * syntax-in-functions.hs: Haskell pattern mathching is nice

--- syntax-in-functions.hs
+++ syntax-in-functions.hs
@@ -34,11 +34,22 @@ head' :: [a] -> a
 head' [] = error "empty list"
 head' (x:_) = x
 
-
 tell :: (Show a) => [a] -> String
 tell [] = "This list is empty"
 tell (x:[]) = "The list has one element: " ++ show x
 tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
 tell (x:y:_) = "The list is very long! The first two elements are " ++ show x ++ " and " ++ show y
 
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length' (_:xs) = 1 + length' xs
 
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
+
+
+--as pattern: all@(x:xs)
+capital :: String -> String
+capital "" = "Empty string!"
+capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
