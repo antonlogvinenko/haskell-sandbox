9d18449b526e2004314a4f635e8ba5b01986e2d2
Anton Logvinenko <logvinenko@farpost.com>
2012-05-26 00:05:34 +1100

    * higher-order-functions.hs: deep dive into currying

--- higher-order-functions.hs
+++ higher-order-functions.hs
@@ -1,1 +1,17 @@
+--curried functions. space is sort of operator and has the highest precedence
+max2 = max 5
 
+multiplyThree :: (Num a) => a -> a -> a -> a
+multiplyThree x y z = x * y * z
+
+multiplyByNine :: (Num a) => a -> a -> a
+multiplyByNine = multiplyThree 9
+
+compareWithHundred :: (Ord a, Num a) => a -> Ordering
+compareWithHundred = compare 100
+
+divideByTen :: (Floating a) => a -> a
+divideByTen = (/ 10)
+
+isUpperAlfanum :: Char -> Bool
+isUpperAlfanum = (`elem` ['A'..'Z'])
