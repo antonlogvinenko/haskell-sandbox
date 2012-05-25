ec6f19b55b3d2943a8a692beea9fed38affe0829
Anton Logvinenko <logvinenko@farpost.com>
2012-05-25 13:57:29 +1100

    * recursion.hs: playing with haskell recursion

--- recursion.hs
+++ recursion.hs
@@ -1,1 +1,18 @@
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of an empty list undefined"
+maximum' [x] = x
+maximum' (x:xs)
+         | x > maxTail = x
+         | otherwise = maxTail
+         where maxTail = maximum' xs
+
+maximum'' :: (Ord a) => [a] -> a
+maximum'' [] = error "maximum of an empty list undefined"
+maximum'' [x] = x
+maximum'' (x:xs) = max x $ maximum'' xs
+
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+           | n <= 0 = []
+           | otherwise = x:replicate' (n-1) x
 
