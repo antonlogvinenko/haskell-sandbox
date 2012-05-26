a321183e1fc97c4e6d834538d6ef0dd92a047a95
Anton Logvinenko <logvinenko@farpost.com>
2012-05-26 18:39:09 +1100

    my own haskell maps and filters

--- higher-order-functions.hs
+++ higher-order-functions.hs
@@ -33,3 +33,20 @@ flip2 :: (a -> b -> c) -> (b -> a -> c)
 flip2 f x y = f y x
 
 --maps and filters
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' f (x:xs)
+	| f x = x : others
+	| otherwise = others
+	where others = filter' f xs
+
+quicksort' :: (Ord a) => [a] -> [a]
+quicksort' [] = []
+quicksort' (x:xs) =
+	let lessThanX = filter' (<= x) xs
+	    moreThanX = [y | y <- xs, y > x ]
+	in lessThanX ++ [x] ++ moreThanX
