d1432ab1ef1a925fa7d93637aa268e3e46bab33b
Anton Logvinenko <logvinenko@farpost.com>
2012-05-27 16:49:30 +1100

    * modules.hs: learning data.list

--- modules.hs
+++ modules.hs
@@ -11,3 +11,21 @@ numUniques = length . nub
 --Data.List
 intersperse1 = intersperse '.' "monkeys"
 intercalate1 = intercalate " " ["hey", "there", "gals"]
+transpose1 = transpose [[1,2,3], [5,6,7], [8,9,0]]
+--foldl', foldl1' strict versions of their lazy incarnations
+concat1 = concat ["cake ", "is ", "a ", "lie"]
+concatMap1 = concatMap (replicate 4) [1..3]
+
+--I'll try to implement it
+concatMap' :: (a -> [b]) -> [a] -> [b]
+concatMap' f xs = concat $ map f xs
+--concatMap' f = concat . map f
+--concatMap' f xs = concat (map f xs)
+
+and1 = and $ map (>4) [4, 6]
+and2 = and $ map (>4) [9, 9]
+--or is analogous
+--any, all - same with predicate
+
+
+
