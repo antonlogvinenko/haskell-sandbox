9b605e515ab77b58cfbe867e5f8245585239f31a
Anton Logvinenko <logvinenko@farpost.com>
2013-01-02 23:23:04 +1100

    A bit of comments

--- input-and-output.hs
+++ input-and-output.hs
@@ -2,16 +2,18 @@
 main1 = putStrLn "Hello, world!"
 -- :t putStrLn has a type of IO ()
 -- IO is an action with some result type, () in this case
--- IO is run when given a name main and the program is run
+-- IO is run when given a name main and run OR when inside another IO action composed with do syntax OR in when in ghci
 
 --do syntax
 --each step is an IO action
---result of IO action is the result of the last step
+--result of IO action is the result of the last step, no binding there
 -- <- binds IO action result to a name
 main2 = do
   putStrLn "Hello, what's your name?"
   name <- getLine
   putStrLn ("Hey " ++ name ++ ", you rock!")
+
+             
 
 main3 = do
   putStrLn "Hello, what's your name?"
@@ -20,6 +22,10 @@ main3 = do
 
 tellFortune :: String -> String
 tellFortune name = "You'll become a rich and successful man!"
+
+-- The following is invalid: the second argument has a type of IO String
+-- So impure environment required to extract the value
+-- nameTag = "Hello, my name is " ++ getLine
 
 main10 = do
   line <- getLine
