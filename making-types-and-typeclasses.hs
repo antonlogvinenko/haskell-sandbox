94fb91bc270d6a6f161f5430e938690b88d4ce97
Anton Logvinenko <logvinenko@farpost.com>
2013-01-02 14:08:57 +1100

    YesNo typeclass

--- making-types-and-typeclasses.hs
+++ making-types-and-typeclasses.hs
@@ -1,3 +1,9 @@
+-- 1. types (with or without parameters), synonyms, deriving typeclasses
+-- 2. typeclasses, subclassing, instantiation
+
+
+
+
 -- **** Algebraic data types
 
 --the type and value constructors
@@ -156,13 +162,33 @@ instance (Eq m) => Eq (Maybe1 m) where
 
 
 -- **** Yes-no typeclass
+class YesNo a where
+    yesno :: a -> Bool
 
+instance YesNo Int where
+    yesno 0 = False
+    yesno _ = True
+
+instance YesNo [a] where
+    yesno [] = False
+    yesno _ = True
+
+instance YesNo Bool where
+    yesno = id
+
+instance YesNo (Maybe a) where
+    yesno (Just _) = True
+    yesno Nothing = False
+
+instance YesNo TrafficLight where 
+    yesno Red = False
+    yesno _ = True
 
 
 
 
 -- **** The Functor typeclass
+
+
 -- **** Kinds
 
--- 1. types (with or without parameters), synonyms, deriving typeclasses
--- 2. typeclasses, subclassing, instantiation
