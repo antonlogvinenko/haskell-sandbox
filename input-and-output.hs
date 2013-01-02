3d8655d6ada6e4c8d6401aa2abb364e7dc49b1e7
Anton Logvinenko <logvinenko@farpost.com>
2013-01-02 22:57:43 +1100

    Haskell IO

--- input-and-output.hs
+++ input-and-output.hs
@@ -1,7 +1,27 @@
+-- **** Hello world
+main1 = putStrLn "Hello, world!"
+-- :t putStrLn has a type of IO ()
+-- IO is an action with some result type, () in this case
+-- IO is run when given a name main and the program is run
 
+--do syntax
+--each step is an IO action
+--result of IO action is the result of the last step
+-- <- binds IO action result to a name
+main2 = do
+  putStrLn "Hello, what's your name?"
+  name <- getLine
+  putStrLn ("Hey " ++ name ++ ", you rock!")
 
+main3 = do
+  putStrLn "Hello, what's your name?"
+  name <- getLine
+  putStrLn $ "Read this carefully, because this is your future: " ++ tellFortune name
 
-main1 = do
+tellFortune :: String -> String
+tellFortune name = "You'll become a rich and successful man!"
+
+main10 = do
   line <- getLine
   if null line
      then return ()
