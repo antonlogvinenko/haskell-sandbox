655ab83d8f5873167aefe43012164adb101d5900
Anton Logvinenko <logvinenko@farpost.com>
2013-01-04 20:37:27 +1100

    Heathrow to London solved

--- functionally-solving-problems.hs
+++ functionally-solving-problems.hs
@@ -21,3 +21,41 @@ solveRPN = head . foldl folding [] . words
 
 
 -- **** Heathrow to London
+data Section = Section {getA :: Int, getB :: Int, getC :: Int} deriving (Show)
+type RoadSystem = [Section]
+
+heathrowToLondon :: RoadSystem
+heathrowToLondon = [Section 50 10 30, Section 5 90 20, Section 40 2 25, Section 10 8 0]
+
+data Label = A | B | C deriving (Show)
+type Path = [(Label, Int)]
+
+-- should return [(B, 10), (C, 30), (A, 5), (C, 20), (B, 2), (B, 8)]
+
+roadStep :: (Path, Path) -> Section -> (Path, Path)
+roadStep (pathA, pathB) (Section a b c) =
+    let priceA = sum $ map snd pathA
+        priceB = sum $ map snd pathB
+        forwardPathToA = priceA + a
+        crossPathToA = priceB + b + c
+        forwardPathToB = priceB + b
+        crossPathToB = priceA + a + c
+        newPathToA = if forwardPathToA <= crossPathToA
+                     then (A, a) : pathA
+                     else (C, c) : (B, b) : pathB
+        newPathToB = if forwardPathToB <= crossPathToB
+                     then (B, b) : pathB
+                     else (C, c) : (A, a) : pathA
+    in (newPathToA, newPathToB)
+
+
+optimalPath :: RoadSystem -> Path
+optimalPath roadSystem =
+    let (bestAPath, bestBPath) = foldl roadStep ([], []) roadSystem
+        a = sum $ map snd bestAPath
+        b = sum $ map snd bestBPath
+    in if a <= b
+       then reverse bestAPath
+       else reverse bestBPath
+
+findOptimalPath = optimalPath heathrowToLondon
