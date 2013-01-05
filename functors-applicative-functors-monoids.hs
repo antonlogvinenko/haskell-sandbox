6b1d8b4c2cc76f18b4f6b4c10bf3acc2c4c33787
Anton Logvinenko <logvinenko@farpost.com>
2013-01-05 13:33:13 +1100

    IO as a functor

--- functors-applicative-functors-monoids.hs
+++ functors-applicative-functors-monoids.hs
@@ -1,1 +1,25 @@
+import Data.Char  
+import Data.List 
 
+-- **** Functors redux
+
+-- IO is a Functor
+-- instance Functor IO where
+--    fmap f action = do
+--      result <- action
+--      return (f result)
+
+main1 = do
+  line <- fmap reverse getLine
+  putStrLn $ "You said " ++ line ++ " backwards!"
+  putStrLn $ "Yes, you really said " ++ line ++ " backwards!"
+
+main2 = do
+  line <- fmap (intersperse '-' . reverse . map toUpper) getLine
+  putStrLn line
+
+
+
+-- Applicative functors
+-- The newtype keyword
+-- Monoids
