5881e6750a1bb996ddae3478bd1a7347c4a91010
Anton Logvinenko <logvinenko@farpost.com>
2013-01-05 14:12:06 +1100

    Functions are functors too

--- functors-applicative-functors-monoids.hs
+++ functors-applicative-functors-monoids.hs
@@ -1,9 +1,13 @@
 import Data.Char  
 import Data.List 
+--import Control.Monad.Instances
 
 -- **** Functors redux
 
--- IO is a Functor
+-- class Functor f where
+--     fmap :: (a -> b) -> f a -> f b
+
+-- IO as a Functor
 -- instance Functor IO where
 --    fmap f action = do
 --      result <- action
@@ -18,6 +22,14 @@ main2 = do
   line <- fmap (intersperse '-' . reverse . map toUpper) getLine
   putStrLn line
 
+-- Function as a Functor
+-- instance Functor ((->) r) where
+--     fmap f g = (\x -> f (g x))
+
+instance Functor ((->) r) where
+    fmap = (.)
+
+main3 = fmap (*3) (+100)
 
 
 -- Applicative functors
