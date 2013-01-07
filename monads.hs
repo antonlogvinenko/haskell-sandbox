fe76bc68d816b0b82135ec5f6fad836f3d83af0c
Anton Logvinenko <logvinenko@farpost.com>
2013-01-07 15:41:07 +1100

    Monad chapters:

--- monads.hs
+++ monads.hs
@@ -1,1 +1,20 @@
+-- If you have a value with a context, `m a`, how do you apply to it
+-- a function that takes a normal `a` and returns a value with a context?
 
+-- (>>=) :: (Monad m) => m a -> (a -> m b) -> m b
+-- Monads are applicative funcotrs that support >>= operation (bind)
+
+-- **** Getting out feet with Monad
+
+
+-- **** The Monad type class
+
+
+-- **** Walk the line (example)
+
+
+-- **** do notation
+
+-- **** The list monad
+
+-- **** Monad laws
